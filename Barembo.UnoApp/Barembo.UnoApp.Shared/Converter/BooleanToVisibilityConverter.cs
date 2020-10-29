62139649de59771ee0e91b020fe49e347ef3dad1
TopperDEL <github_com@tparth.de>
2020-10-29 13:19:21 +0100

    feat: BooleanVisibilityConverter

---
+++ Barembo.UnoApp/Barembo.UnoApp.Shared/Converter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Barembo.UnoApp.Shared.Converter
+{
+    class BooleanToVisibilityConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if ((bool)value)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
