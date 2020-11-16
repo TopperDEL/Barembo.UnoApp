6a6d0fbb26aecb64f1a683afb8a0e8536dd5661f
TopperDEL <github_com@tparth.de>
2020-11-16 19:22:31 +0100

    build: Next try updating Nuget on AppCenter

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,2 +1,2 @@
 Write-Host “RUNNING POST-CLONE”
-Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile Nuget.exe
+Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile "C:\hostedtoolcache\windows\NuGet\4.9.4\x64\Nuget.exe"
