e301ad2bac3660e9a4c1d05c4887b1c87b8fd9f6
TopperDEL <github_com@tparth.de>
2020-12-26 21:36:41 +0100

    feat: AddMediaButton

---
+++ Barembo.UnoApp/Barembo.UnoApp.Shared/Services/MediaFetchService.cs
@@ -0,0 +1,25 @@
+﻿using Barembo.App.Core.Messages;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barembo.UnoApp.Shared.Services
+{
+    public class MediaFetchService
+    {
+        readonly IEventAggregator _eventAggregator;
+
+        public MediaFetchService(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+
+            _eventAggregator.GetEvent<MediaRequestedMessage>().Subscribe(HandleMediaRequested);
+        }
+
+        private void HandleMediaRequested(MediaResult result)
+        {
+
+        }
+    }
+}
